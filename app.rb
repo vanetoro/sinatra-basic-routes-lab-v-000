a7bd1f866ee836231853c85d4183f1eddc595409
Vanessa Toro <vanetoro84@gmail.com>
2018-05-18 22:36:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is Vane'
+  end
+
+  get '/hometown' do
+    'My hometown is Miami'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is Days'
+  end
+
 end
