6a145026e58fed6d86fa49b235ad924c6f36bf56
Vanessa Toro <vanetoro84@gmail.com>
2018-05-18 22:29:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    'Hello Vane!'
+  get 'name' do
+    'My name is'
+  end
+
+  get 'hometown' do
+    'My hometown is'
+  end
+
+  get 'favorite-song' do
+    'My favorite song is'
   end
 end
